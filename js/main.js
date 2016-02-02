f8553e3a72e4aefbd8a5ebea86938e5af2c8d9b4
Graphic <graphic@SOD-SJH-605-04M.local>
2016-02-02 16:36:43 -0500

    project completed

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
 
 
+
+
+$('button').on('click', function(){
+	$('.on').toggleClass('off');
+
+if ($('button').hasClass('off')){
+	$('body').addClass('dark');
+	$('.status').text('Hey, who turned off th light?!');
+}
+
+else {
+	$('body').removeClass('dark');
+	$('.status').text('Its so bright in here!');
+}
+
+});
+
+
+
